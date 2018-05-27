30e68cf05d27eec8e0696918b080d85d7ac6a30d
HAFFAD MOURAD <haffadmourad86@gmail.com>
2018-05-27 12:59:51 +0100

    User list created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {path: 'appComponent', redirectTo : '/', pathMatch: 'full'},
-  {path: 'appComponent', component: AppComponent }
+  {path: 'appComponent', component: AppComponent },
+  {path: 'users', component: UserComponent }
 ];
 
 @NgModule({
